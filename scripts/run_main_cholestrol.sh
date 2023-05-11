30afa01d304a69b2b7a05a026776ab652c5565ff
tmddus49 <oox1987@kaist.ac.kr>
2023-05-11 22:07:45 +0900

    MAE with TTA method - main_mae_method()

---
+++ scripts/run_main_cholestrol.sh
@@ -0,0 +1,224 @@
+python main.py \
+    meta_dataset=openml-regression \
+    dataset=cholestrol \
+    shift_type=null \
+    retrain=true \
+    seed=0 \
+    out_dir=exps/cholestrol \
+
+python main.py \
+    meta_dataset=openml-regression \
+    dataset=cholestrol \
+    shift_type=null \
+    retrain=false \
+    seed=1 \
+    out_dir=exps/cholestrol \
+
+python main.py \
+    meta_dataset=openml-regression \
+    dataset=cholestrol \
+    shift_type=null \
+    retrain=false \
+    seed=2 \
+    out_dir=exps/cholestrol \
+
+
+
+python main.py \
+    meta_dataset=openml-regression \
+    dataset=cholestrol \
+    shift_type=Gaussian \
+    shift_severity=1 \
+    seed=0 \
+    out_dir=exps/cholestrol \
+
+python main.py \
+    meta_dataset=openml-regression \
+    dataset=cholestrol \
+    shift_type=Gaussian \
+    shift_severity=1 \
+    seed=1 \
+    out_dir=exps/cholestrol \
+
+python main.py \
+    meta_dataset=openml-regression \
+    dataset=cholestrol \
+    shift_type=Gaussian \
+    shift_severity=1 \
+    seed=2 \
+    out_dir=exps/cholestrol \
+
+
+
+python main.py \
+    meta_dataset=openml-regression \
+    dataset=cholestrol \
+    shift_type=random_drop \
+    shift_severity=0.6 \
+    seed=0 \
+    out_dir=exps/cholestrol \
+
+python main.py \
+    meta_dataset=openml-regression \
+    dataset=cholestrol \
+    shift_type=random_drop \
+    shift_severity=0.6 \
+    seed=1 \
+    out_dir=exps/cholestrol \
+
+python main.py \
+    meta_dataset=openml-regression \
+    dataset=cholestrol \
+    shift_type=random_drop \
+    shift_severity=0.6 \
+    seed=2 \
+    out_dir=exps/cholestrol \
+
+
+
+python main.py \
+    meta_dataset=openml-regression \
+    dataset=cholestrol \
+    shift_type=column_drop \
+    shift_severity=0.6 \
+    seed=0 \
+    out_dir=exps/cholestrol \
+
+python main.py \
+    meta_dataset=openml-regression \
+    dataset=cholestrol \
+    shift_type=column_drop \
+    shift_severity=0.6 \
+    seed=1 \
+    out_dir=exps/cholestrol \
+
+python main.py \
+    meta_dataset=openml-regression \
+    dataset=cholestrol \
+    shift_type=column_drop \
+    shift_severity=0.6 \
+    seed=2 \
+    out_dir=exps/cholestrol \
+
+
+
+python main.py \
+    meta_dataset=openml-regression \
+    dataset=cholestrol \
+    shift_type=mean_shift \
+    shift_severity=1 \
+    seed=0 \
+    out_dir=exps/cholestrol \
+
+python main.py \
+    meta_dataset=openml-regression \
+    dataset=cholestrol \
+    shift_type=mean_shift \
+    shift_severity=1 \
+    seed=1 \
+    out_dir=exps/cholestrol \
+
+python main.py \
+    meta_dataset=openml-regression \
+    dataset=cholestrol \
+    shift_type=mean_shift \
+    shift_severity=1 \
+    seed=2 \
+    out_dir=exps/cholestrol \
+
+
+
+python main.py \
+    meta_dataset=openml-regression \
+    dataset=cholestrol \
+    shift_type=std_shift \
+    shift_severity=1 \
+    seed=0 \
+    out_dir=exps/cholestrol \
+
+python main.py \
+    meta_dataset=openml-regression \
+    dataset=cholestrol \
+    shift_type=std_shift \
+    shift_severity=1 \
+    seed=1 \
+    out_dir=exps/cholestrol \
+
+python main.py \
+    meta_dataset=openml-regression \
+    dataset=cholestrol \
+    shift_type=std_shift \
+    shift_severity=1 \
+    seed=2 \
+    out_dir=exps/cholestrol \
+
+
+
+python main.py \
+    meta_dataset=openml-regression \
+    dataset=cholestrol \
+    shift_type=mean_std_shift \
+    shift_severity=1 \
+    seed=0\
+    out_dir=exps/cholestrol \
+
+python main.py \
+    meta_dataset=openml-regression \
+    dataset=cholestrol \
+    shift_type=mean_std_shift \
+    shift_severity=1 \
+    seed=1 \
+    out_dir=exps/cholestrol \
+
+python main.py \
+    meta_dataset=openml-regression \
+    dataset=cholestrol \
+    shift_type=mean_std_shift \
+    shift_severity=1 \
+    seed=2 \
+    out_dir=exps/cholestrol \
+
+
+
+python main.py \
+    meta_dataset=tableshift \
+    dataset=heloc \
+    retrain=true \
+    seed=0 \
+    out_dir=exps/heloc_main \
+
+python main.py \
+    meta_dataset=tableshift \
+    dataset=heloc \
+    retrain=false \
+    seed=1 \
+    out_dir=exps/heloc_main \
+
+python main.py \
+    meta_dataset=tableshift \
+    dataset=heloc \
+    retrain=false \
+    seed=2 \
+    out_dir=exps/heloc_main \
+
+
+python main.py \
+    meta_dataset=tableshift \
+    dataset=diabetes_readmission \
+    retrain=true \
+    seed=0 \
+    out_dir=exps/diabetes_main \
+
+python main.py \
+    meta_dataset=tableshift \
+    dataset=diabetes_readmission \
+    retrain=false \
+    seed=1 \
+    out_dir=exps/diabetes_main \
+
+python main.py \
+    meta_dataset=tableshift \
+    dataset=diabetes_readmission \
+    retrain=false \
+    seed=2 \
+    out_dir=exps/diabetes_main \
