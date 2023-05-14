4168dd8a8292c613be7cd0f8bebb89f512e32197
drumpt <ssohot1@kaist.ac.kr>
2023-05-14 13:51:47 -0400

    Fix run_main.sh

--- scripts/run_main.sh
+++ scripts/run_main.sh
@@ -1,156 +1,156 @@
-# python main.py \
-#     meta_dataset=openml-cc18 \
-#     dataset=cmc \
-#     shift_type=null \
-#     retrain=true \
-#     seed=0 \
-#     out_dir=exps/cmc \
+python main.py \
+    meta_dataset=openml-cc18 \
+    dataset=cmc \
+    shift_type=null \
+    retrain=true \
+    seed=0 \
+    out_dir=exps/cmc \
 
-# python main.py \
-#     meta_dataset=openml-cc18 \
-#     dataset=cmc \
-#     shift_type=null \
-#     retrain=false \
-#     seed=1 \
-#     out_dir=exps/cmc \
+python main.py \
+    meta_dataset=openml-cc18 \
+    dataset=cmc \
+    shift_type=null \
+    retrain=false \
+    seed=1 \
+    out_dir=exps/cmc \
 
-# python main.py \
-#     meta_dataset=openml-cc18 \
-#     dataset=cmc \
-#     shift_type=null \
-#     retrain=false \
-#     seed=2 \
-#     out_dir=exps/cmc \
-
-
-
-# python main.py \
-#     meta_dataset=openml-cc18 \
-#     dataset=cmc \
-#     shift_type=Gaussian \
-#     shift_severity=1 \
-#     seed=0 \
-#     out_dir=exps/cmc \
-
-# python main.py \
-#     meta_dataset=openml-cc18 \
-#     dataset=cmc \
-#     shift_type=Gaussian \
-#     shift_severity=1 \
-#     seed=1 \
-#     out_dir=exps/cmc \
-
-# python main.py \
-#     meta_dataset=openml-cc18 \
-#     dataset=cmc \
-#     shift_type=Gaussian \
-#     shift_severity=1 \
-#     seed=2 \
-#     out_dir=exps/cmc \
+python main.py \
+    meta_dataset=openml-cc18 \
+    dataset=cmc \
+    shift_type=null \
+    retrain=false \
+    seed=2 \
+    out_dir=exps/cmc \
 
 
 
-# python main.py \
-#     meta_dataset=openml-cc18 \
-#     dataset=cmc \
-#     shift_type=random_drop \
-#     shift_severity=0.6 \
-#     seed=0 \
-#     out_dir=exps/cmc \
+python main.py \
+    meta_dataset=openml-cc18 \
+    dataset=cmc \
+    shift_type=Gaussian \
+    shift_severity=1 \
+    seed=0 \
+    out_dir=exps/cmc \
 
-# python main.py \
-#     meta_dataset=openml-cc18 \
-#     dataset=cmc \
-#     shift_type=random_drop \
-#     shift_severity=0.6 \
-#     seed=1 \
-#     out_dir=exps/cmc \
+python main.py \
+    meta_dataset=openml-cc18 \
+    dataset=cmc \
+    shift_type=Gaussian \
+    shift_severity=1 \
+    seed=1 \
+    out_dir=exps/cmc \
 
-# python main.py \
-#     meta_dataset=openml-cc18 \
-#     dataset=cmc \
-#     shift_type=random_drop \
-#     shift_severity=0.6 \
-#     seed=2 \
-#     out_dir=exps/cmc \
-
-
-
-# python main.py \
-#     meta_dataset=openml-cc18 \
-#     dataset=cmc \
-#     shift_type=column_drop \
-#     shift_severity=0.6 \
-#     seed=0 \
-#     out_dir=exps/cmc \
-
-# python main.py \
-#     meta_dataset=openml-cc18 \
-#     dataset=cmc \
-#     shift_type=column_drop \
-#     shift_severity=0.6 \
-#     seed=1 \
-#     out_dir=exps/cmc \
-
-# python main.py \
-#     meta_dataset=openml-cc18 \
-#     dataset=cmc \
-#     shift_type=column_drop \
-#     shift_severity=0.6 \
-#     seed=2 \
-#     out_dir=exps/cmc \
+python main.py \
+    meta_dataset=openml-cc18 \
+    dataset=cmc \
+    shift_type=Gaussian \
+    shift_severity=1 \
+    seed=2 \
+    out_dir=exps/cmc \
 
 
 
-# python main.py \
-#     meta_dataset=openml-cc18 \
-#     dataset=cmc \
-#     shift_type=random_replacement \
-#     shift_severity=0.6 \
-#     seed=0 \
-#     out_dir=exps/cmc \
+python main.py \
+    meta_dataset=openml-cc18 \
+    dataset=cmc \
+    shift_type=random_drop \
+    shift_severity=0.6 \
+    seed=0 \
+    out_dir=exps/cmc \
 
-# python main.py \
-#     meta_dataset=openml-cc18 \
-#     dataset=cmc \
-#     shift_type=random_replacement \
-#     shift_severity=0.6 \
-#     seed=1 \
-#     out_dir=exps/cmc \
+python main.py \
+    meta_dataset=openml-cc18 \
+    dataset=cmc \
+    shift_type=random_drop \
+    shift_severity=0.6 \
+    seed=1 \
+    out_dir=exps/cmc \
 
-# python main.py \
-#     meta_dataset=openml-cc18 \
-#     dataset=cmc \
-#     shift_type=random_replacement \
-#     shift_severity=0.6 \
-#     seed=2 \
-#     out_dir=exps/cmc \
+python main.py \
+    meta_dataset=openml-cc18 \
+    dataset=cmc \
+    shift_type=random_drop \
+    shift_severity=0.6 \
+    seed=2 \
+    out_dir=exps/cmc \
 
 
 
-# python main.py \
-#     meta_dataset=openml-cc18 \
-#     dataset=cmc \
-#     shift_type=column_replacement \
-#     shift_severity=0.6 \
-#     seed=0 \
-#     out_dir=exps/cmc \
+python main.py \
+    meta_dataset=openml-cc18 \
+    dataset=cmc \
+    shift_type=column_drop \
+    shift_severity=0.6 \
+    seed=0 \
+    out_dir=exps/cmc \
 
-# python main.py \
-#     meta_dataset=openml-cc18 \
-#     dataset=cmc \
-#     shift_type=column_replacement \
-#     shift_severity=0.6 \
-#     seed=1 \
-#     out_dir=exps/cmc \
+python main.py \
+    meta_dataset=openml-cc18 \
+    dataset=cmc \
+    shift_type=column_drop \
+    shift_severity=0.6 \
+    seed=1 \
+    out_dir=exps/cmc \
 
-# python main.py \
-#     meta_dataset=openml-cc18 \
-#     dataset=cmc \
-#     shift_type=column_replacement \
-#     shift_severity=0.6 \
-#     seed=2 \
-#     out_dir=exps/cmc \
+python main.py \
+    meta_dataset=openml-cc18 \
+    dataset=cmc \
+    shift_type=column_drop \
+    shift_severity=0.6 \
+    seed=2 \
+    out_dir=exps/cmc \
+
+
+
+python main.py \
+    meta_dataset=openml-cc18 \
+    dataset=cmc \
+    shift_type=random_replacement \
+    shift_severity=0.6 \
+    seed=0 \
+    out_dir=exps/cmc \
+
+python main.py \
+    meta_dataset=openml-cc18 \
+    dataset=cmc \
+    shift_type=random_replacement \
+    shift_severity=0.6 \
+    seed=1 \
+    out_dir=exps/cmc \
+
+python main.py \
+    meta_dataset=openml-cc18 \
+    dataset=cmc \
+    shift_type=random_replacement \
+    shift_severity=0.6 \
+    seed=2 \
+    out_dir=exps/cmc \
+
+
+
+python main.py \
+    meta_dataset=openml-cc18 \
+    dataset=cmc \
+    shift_type=column_replacement \
+    shift_severity=0.6 \
+    seed=0 \
+    out_dir=exps/cmc \
+
+python main.py \
+    meta_dataset=openml-cc18 \
+    dataset=cmc \
+    shift_type=column_replacement \
+    shift_severity=0.6 \
+    seed=1 \
+    out_dir=exps/cmc \
+
+python main.py \
+    meta_dataset=openml-cc18 \
+    dataset=cmc \
+    shift_type=column_replacement \
+    shift_severity=0.6 \
+    seed=2 \
+    out_dir=exps/cmc \
 
 
 
@@ -232,45 +232,45 @@ python main.py \
 
 
 
-# python main.py \
-#     meta_dataset=tableshift \
-#     dataset=heloc \
-#     retrain=true \
-#     seed=0 \
-#     out_dir=exps/heloc_main \
+python main.py \
+    meta_dataset=tableshift \
+    dataset=heloc \
+    retrain=true \
+    seed=0 \
+    out_dir=exps/heloc_main \
 
-# python main.py \
-#     meta_dataset=tableshift \
-#     dataset=heloc \
-#     retrain=false \
-#     seed=1 \
-#     out_dir=exps/heloc_main \
+python main.py \
+    meta_dataset=tableshift \
+    dataset=heloc \
+    retrain=false \
+    seed=1 \
+    out_dir=exps/heloc_main \
 
-# python main.py \
-#     meta_dataset=tableshift \
-#     dataset=heloc \
-#     retrain=false \
-#     seed=2 \
-#     out_dir=exps/heloc_main \
+python main.py \
+    meta_dataset=tableshift \
+    dataset=heloc \
+    retrain=false \
+    seed=2 \
+    out_dir=exps/heloc_main \
 
 
-# python main.py \
-#     meta_dataset=tableshift \
-#     dataset=diabetes_readmission \
-#     retrain=true \
-#     seed=0 \
-#     out_dir=exps/diabetes_main \
+python main.py \
+    meta_dataset=tableshift \
+    dataset=diabetes_readmission \
+    retrain=true \
+    seed=0 \
+    out_dir=exps/diabetes_main \
 
-# python main.py \
-#     meta_dataset=tableshift \
-#     dataset=diabetes_readmission \
-#     retrain=false \
-#     seed=1 \
-#     out_dir=exps/diabetes_main \
+python main.py \
+    meta_dataset=tableshift \
+    dataset=diabetes_readmission \
+    retrain=false \
+    seed=1 \
+    out_dir=exps/diabetes_main \
 
-# python main.py \
-#     meta_dataset=tableshift \
-#     dataset=diabetes_readmission \
-#     retrain=false \
-#     seed=2 \
-#     out_dir=exps/diabetes_main \
+python main.py \
+    meta_dataset=tableshift \
+    dataset=diabetes_readmission \
+    retrain=false \
+    seed=2 \
+    out_dir=exps/diabetes_main \
