4c597757669fcec431440944c71b3cb300a4abe1
drumpt <ssohot1@kaist.ac.kr>
2024-08-13 22:20:09 +0900

    Finish Implementing parsing code

--- scripts/parse_result_aaai.py
+++ scripts/parse_result_aaai.py
@@ -82,8 +82,8 @@ def parse_common_corruption():
     ]
 
     for model in MODEL_LIST:
-        for dataset in DATASETS:
-            for method in METHOD_LIST:
+        for method in METHOD_LIST:
+            for dataset in DATASETS:
                 result_list = []
                 for i in range(len(SHIFT_LIST)):
                     shift_type = SHIFT_LIST[i]
@@ -113,12 +113,142 @@ def parse_common_corruption():
                         )
                     except:
                         mean_list, sem_list = [np.nan] * 6, [np.nan] * 6
-                    temp_result_list = [f"{mean * 100:.1f} ± {sem * 100:.1f}" for mean, sem in zip(mean_list, sem_list)]
+                    # temp_result_list = [f"{mean * 100:.1f} ± {sem * 100:.1f}" for mean, sem in zip(mean_list, sem_list)]
+                    temp_result_list = [f"{mean * 100:.1f}" for mean, sem in zip(mean_list, sem_list)]
                     result_list.append(temp_result_list)
                 result_df = pd.DataFrame(result_list, columns=["Acc.", "bAcc.", "F1", "Acc.", "bAcc.", "F1"], index=SHIFT_LIST)
                 print(f"{model=} {dataset=} {method=}")
                 print(f"{result_df=}\n")
 
 
+
+def parse_various_architectures():
+    LOG_DIR = "log"
+    LOG_PREFIX = "common_corruption"
+
+    SEEDS = [0, 1, 2]
+    MODEL_LIST = ["ResNet", "AutoInt", "TabNet", "FTTransformer"]
+    # MODEL_LIST = ["ResNet"]
+    benchmark = "tableshift"
+    DATASETS = ["anes", "heloc", "nhanes_lead", "diabetes_readmission"]
+    # DATASETS = ["anes"]
+    shift_type = "None"
+    shift_severity = 0
+
+    METHOD_LIST = [
+        "calibrator_label_distribution_handler",
+        "pl",
+        "tent",
+        "eata",
+        "sar",
+        "lame",
+    ]
+
+    for model in MODEL_LIST:
+        result_list = []
+        for method in METHOD_LIST:
+            temp_result_list = []
+            for dataset in DATASETS:
+                stochastic_result_list = []
+                for seed in SEEDS:
+                    log_dir = f"{LOG_DIR}/{LOG_PREFIX}/{benchmark}_{dataset}_shift_type_{shift_type}_shift_severity_{shift_severity}_{model}_{method}_seed_{seed}.txt"
+
+                    result = parse(log_dir)
+                    stochastic_result_list.append(
+                        [
+                            result["test_acc_before"],
+                            result["test_bacc_before"],
+                            result["test_f1_before"],
+                            result["test_acc_after"],
+                            result["test_bacc_after"],
+                            result["test_f1_after"],
+                        ]
+                    )
+                stochastic_result = np.array(
+                    stochastic_result_list, dtype=np.float32
+                )
+                try:
+                    mean_list = np.mean(stochastic_result, axis=0)
+                    sem_list = np.std(stochastic_result, axis=0, ddof=1) / np.sqrt(
+                        stochastic_result.shape[-1]
+                    )
+                except:
+                    mean_list, sem_list = [np.nan] * 6, [np.nan] * 6
+                temp_result = [[f"{mean * 100:.1f}" for mean, sem in zip(mean_list, sem_list)][-4], [f"{mean * 100:.1f}" for mean, sem in zip(mean_list, sem_list)][-1]]
+                temp_result_list.append(temp_result)
+            print(f"{temp_result_list=}")
+            result_list.append(np.mean(temp_result_list, axis=0))
+        result_df = pd.DataFrame(result_list, columns=["F1", "F1"], index=METHOD_LIST)
+        print(f"{model=} {dataset=} {method=}")
+        print(f"{result_df=}\n")
+
+
+
+def parse_harsh_condition():
+    LOG_DIR = "log"
+    LOG_PREFIX = "common_corruption"
+
+    SEEDS = [0, 1, 2]
+    MODEL_LIST = ["MLP"]
+    benchmark = "tableshift"
+    DATASETS = ["anes", "heloc", "nhanes_lead", "diabetes_readmission"]
+    SHIFT_LIST = [
+        "temp_corr",
+        "imbalanced",
+    ]
+    SEVERITY_LIST = [0.1, 0.1]
+    METHOD_LIST = [
+        "calibrator_label_distribution_handler",
+        "pl",
+        "tent",
+        "eata",
+        "sar",
+        "lame",
+    ]
+
+    for model in MODEL_LIST:
+        for method in METHOD_LIST:
+            for dataset in DATASETS:
+                result_list = []
+                for i in range(len(SHIFT_LIST)):
+                    shift_type = SHIFT_LIST[i]
+                    shift_severity = SEVERITY_LIST[i]
+                    stochastic_result_list = []
+                    for seed in SEEDS:
+                        log_dir = f"{LOG_DIR}/{LOG_PREFIX}/{benchmark}_{dataset}_shift_type_{shift_type}_shift_severity_{shift_severity}_{model}_{method}_seed_{seed}.txt"
+                        print(f"{log_dir=} {os.path.exists(log_dir)=}")
+                        result = parse(log_dir)
+
+                        stochastic_result_list.append(
+                            [
+                                result["test_acc_before"],
+                                result["test_bacc_before"],
+                                result["test_f1_before"],
+                                result["test_acc_after"],
+                                result["test_bacc_after"],
+                                result["test_f1_after"],
+                            ]
+                        )
+                    stochastic_result = np.array(
+                        stochastic_result_list, dtype=np.float32
+                    )
+                    try:
+                        mean_list = np.mean(stochastic_result, axis=0)
+                        sem_list = np.std(stochastic_result, axis=0, ddof=1) / np.sqrt(
+                            stochastic_result.shape[-1]
+                        )
+                    except:
+                        mean_list, sem_list = [np.nan] * 6, [np.nan] * 6
+                    # temp_result_list = [f"{mean * 100:.1f} ± {sem * 100:.1f}" for mean, sem in zip(mean_list, sem_list)]
+                    temp_result_list = [f"{mean * 100:.1f}" for mean, sem in zip(mean_list, sem_list)]
+                    result_list.append(temp_result_list)
+                result_df = pd.DataFrame(result_list, columns=["Acc.", "bAcc.", "F1", "Acc.", "bAcc.", "F1"], index=SHIFT_LIST)
+                print(f"{model=} {dataset=} {method=}")
+                print(f"{result_df=}\n")
+
+
+
 if __name__ == "__main__":
-    parse_common_corruption()
+    # parse_common_corruption()
+    # parse_various_architectures()
+    parse_harsh_condition()
